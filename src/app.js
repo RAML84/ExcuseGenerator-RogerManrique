0b9c68a69ab70fb3c2b75ffde2397d32126c66a9
Roger Manrique <159472606+RAML84@users.noreply.github.com>
2024-04-11 15:11:37 +0000

    excuse-generator-RogerManrique

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+let excuseGenerator = (who, action, what, when) => {
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  let excuse = `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+  return excuse;
+};
+
+window.onload = () => {
+  const excuse = excuseGenerator(who, action, what, when);
+  document.getElementById("excuse").innerHTML = excuse;
 };
